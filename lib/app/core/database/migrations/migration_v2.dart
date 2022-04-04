bce859c675c15d9af984660b19c704ada5b6caa0
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-04 08:12:29 -0300

    sql connection

---
+++ lib/app/core/database/migrations/migration_v2.dart
@@ -0,0 +1,20 @@
+
+import 'package:sqflite_common/sqlite_api.dart';
+
+import 'migration.dart';
+
+class MigrationV2 implements Migration{
+  @override
+  void create(Batch batch) {
+    batch.execute('''
+      create table teste(id integer)
+      )
+    ''');
+  }
+
+  @override
+  void update(Batch batch) {
+    // TODO: implement update
+  }
+  
+}
---
+++ lib/app/core/database/sqlite_migration_factory.dart
@@ -0,0 +1,21 @@
+import 'package:todo_list/app/core/database/migrations/migration.dart';
+import 'package:todo_list/app/core/database/migrations/migration_v1.dart';
+
+import 'migrations/migration_v2.dart';
+
+class SqliteMigrationFactory {
+
+  List<Migration> getCreateMigration() => [
+    MigrationV1(),
+  ];
+
+  List<Migration> getUpgradeMigration(int version) {
+    var migrations = <Migration>[];
+
+    if(version == 1) {
+      migrations.add(MigrationV2());
+    }
+
+    return migrations;
+  }
+}
