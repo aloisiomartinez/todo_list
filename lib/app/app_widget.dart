79f37c21f25243e88a0c745d423afee06630cf83
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-08 20:01:46 -0300

    firebase configuration with flutter

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/database/sqlite_adm_connection.dart';
@@ -20,6 +22,7 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   void initState() {
     super.initState();
+    FirebaseAuth auth = FirebaseAuth.instance;
     WidgetsBinding.instance?.addObserver(sqliteAdmConnection);
   }
 
