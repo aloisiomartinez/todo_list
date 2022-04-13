3493e953bc81007061d9ed3d201bf6c4f3932b82
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-13 20:53:10 -0300

    loaders, errors centralized

--- lib/app/modules/auth/register/register_page.dart
+++ lib/app/modules/auth/register/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_list/app/core/notifier/default_listener_notifier.dart';
 import 'package:todo_list/app/core/ui/theme_extensions.dart';
 import 'package:todo_list/app/core/validators/validators.dart';
 import 'package:todo_list/app/core/widget/todo_list_field.dart';
@@ -31,29 +32,20 @@ class _RegisterPageState extends State<RegisterPage> {
     _emailEC.dispose();
     _passwordEC.dispose();
     _confirmPasswordEC.dispose();
-    context.read<RegisterController>().removeListener(() { });
     super.dispose();
   }
 
   @override
   void initState() {
    super.initState();
-   context.read<RegisterController>().addListener(() { 
-     final controller = context.read<RegisterController>();
-     var success = controller.success;
-     var error = controller.error;
-
-     if(success) {
+   var defaultListener = DefaultListenerNotifier(changeNotifier: context.read<RegisterController>());
+   defaultListener.listener(
+     context: context, 
+     successCallback: (notifier, listenerInstance) {
+       listenerInstance.dispose();
        Navigator.of(context).pop();
-     } else if(error != null && error.isNotEmpty){
-       ScaffoldMessenger.of(context).showSnackBar(
-         SnackBar(
-           content: Text(error),
-           backgroundColor: Colors.red, 
-          ),
-       );
-     }
-   });
+     }, 
+   );
   }
 
   @override
