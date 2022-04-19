c6b3df344dda6f1ef21c198a312b99298c3e2c42
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-19 08:13:41 -0300

    identifying if user is authenticated

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/app_widget.dart';
+import 'package:todo_list/app/core/auth/auth_provider.dart';
 import 'package:todo_list/app/core/database/sqlite_connection_factory.dart';
 import 'package:todo_list/app/repositories/user/user_repository.dart';
 import 'package:todo_list/app/repositories/user/user_repository_impl.dart';
@@ -9,7 +10,7 @@ import 'package:todo_list/app/services/user/user_service.dart';
 import 'package:todo_list/app/services/user/user_service_impl.dart';
 
 class AppModule extends StatelessWidget {
-  const AppModule({ Key? key }) : super(key: key);
+  const AppModule({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +25,16 @@ class AppModule extends StatelessWidget {
           create: (context) => UserRepositoryImpl(firebaseAuth: context.read()),
         ),
         Provider<UserService>(
-          create: (context) => UserServiceImpl(userRepository: context.read())
+            create: (context) =>
+                UserServiceImpl(userRepository: context.read())),
+        ChangeNotifierProvider(
+          create: (context) => AuthProvider(
+              firebaseAuth: context.read(), userService: context.read())
+            ..loadListener(),
+            lazy: false,
         )
       ],
-      child: AppWidget() ,
+      child: AppWidget(),
     );
   }
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/app/core/auth/auth_provider.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -8,7 +10,12 @@ class HomePage extends StatelessWidget {
    Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(title: const Text('HomePage'),),
-           body: Container(),
+           body: Center(child: TextButton(
+             onPressed: () {
+               context.read<AuthProvider>().logout();
+             },
+             child: Text('Logout'),
+           ),)
        );
   }
 }
