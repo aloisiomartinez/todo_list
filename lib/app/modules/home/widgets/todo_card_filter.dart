e3d74d30dce3ba4d4980fce9cd8afc684969dfbd
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-20 14:05:23 -0300

    todocard filter

--- lib/app/modules/home/widgets/todo_card_filter.dart
+++ lib/app/modules/home/widgets/todo_card_filter.dart
@@ -24,6 +24,28 @@ class _TodoCardFilterState extends State<TodoCardFilter> {
            border: Border.all(width: 1, color: Colors.grey.withOpacity(.8)),
            borderRadius: BorderRadius.circular(30)
          ),
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             Text('10 TASKS', style: context.titleStyle.copyWith(
+               fontSize: 10,
+               color: Colors.white
+             ),
+            ),
+            Text('HOJE', style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            ),),
+             LinearProgressIndicator(
+               backgroundColor: context.primaryColorLight,
+               valueColor: AlwaysStoppedAnimation<Color>(
+                 Colors.white
+               ),
+               value: 0.4,
+             )
+           ],
+         ),
        );
   }
 }
