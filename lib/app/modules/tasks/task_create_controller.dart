c5f26281ff5ea7313237906ba856016b436d0cfe
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-24 19:48:02 -0300

    starting tasks modules

---
+++ lib/app/modules/tasks/task_create_controller.dart
@@ -0,0 +1,6 @@
+
+import 'package:todo_list/app/core/notifier/default_change_notifier.dart';
+
+class TaskCreateController extends DefaultChangeNotifier{
+  
+}
---
+++ lib/app/modules/tasks/task_create_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TaskCreatePage extends StatelessWidget {
+
+  const TaskCreatePage({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Task'),),
+           body: Container(),
+       );
+  }
+}
