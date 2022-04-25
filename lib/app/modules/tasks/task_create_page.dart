f897de4a4aec10cf1f627269ef538a60ce23a75c
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-04-25 18:27:55 -0300

    calendar button

--- lib/app/modules/tasks/task_create_page.dart
+++ lib/app/modules/tasks/task_create_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/ui/theme_extensions.dart';
+import 'package:todo_list/app/core/widget/todo_list_field.dart';
 import 'package:todo_list/app/modules/tasks/task_create_controller.dart';
+import 'package:todo_list/app/modules/tasks/widgets/calendar_button.dart';
 
 class TaskCreatePage extends StatelessWidget {
   TaskCreateController _controller;
@@ -12,9 +15,45 @@ class TaskCreatePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        elevation: 0,
         title: const Text('Task'),
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: Icon(
+                Icons.close,
+                color: Colors.black,
+              ))
+        ],
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: Text(
+            'Salvar Text',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )),
+      body: Form(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text('Criar Atividade',
+                  style: context.titleStyle.copyWith(fontSize: 20),),
+              ),
+              SizedBox(height: 30,),
+              TodoListField(label: ''),
+              SizedBox(height: 20,),
+              CalendarButton()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
